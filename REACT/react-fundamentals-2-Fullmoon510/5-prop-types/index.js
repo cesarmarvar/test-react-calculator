107eb2bc2f7cf7a1ffa17bd71ffa674c612d547f
Cesar Martinez <cmarvarc@hotmail.com>
2022-06-07 22:17:31 -0500

    Solved 5th assignment all over again

--- REACT/react-fundamentals-2-Fullmoon510/5-prop-types/index.js
+++ REACT/react-fundamentals-2-Fullmoon510/5-prop-types/index.js
@@ -1,28 +1,103 @@
-const root = React.createRoot(document.getElementById("root"))
+// function Button({ size, type, children }) {
+//   const typeClass = type ? `button--${type}` : "";
+//   const sizeClass = size ? `button--${size}` : "";
 
-// Icons
-const mailIcon = (
-  <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M16 4.5L16.0001 4.5C16.3829 4.49993 16.7513 4.64622 17.0297 4.90892C17.2305 5.0984 17.3737 5.33809 17.4462 5.60038L10 9.323L2.55384 5.60038C2.62634 5.33809 2.76945 5.0984 2.97029 4.90892C3.24874 4.64622 3.6171 4.49993 3.99991 4.5H4H16ZM9.77639 12.5652L10 12.677L10.2236 12.5652L17.5 8.92702V14C17.5 14.3978 17.342 14.7794 17.0607 15.0607C16.7794 15.342 16.3978 15.5 16 15.5H4C3.60218 15.5 3.22064 15.342 2.93934 15.0607C2.65804 14.7794 2.5 14.3978 2.5 14V8.92702L9.77639 12.5652Z"
-      fill="currentColor"
-      stroke="currentColor"
-    />
-  </svg>
-);
+//   return (
+//     <button className={`button ${typeClass} ${sizeClass}`}>{children}</button>
+//   );
+// }
 
-// Components
-function Button({ size, type, children }) {
-  const typeClass = type ? `button--${type}` : "";
-  const sizeClass = size ? `button--${size}` : "";
+// Root 
 
-  return (
-    <button className={`button ${typeClass} ${sizeClass}`}>{children}</button>
-  );
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// // Icon component
+
+// function Icon(props) {
+//   const { fill = true, name } = props
+//   const fillIcon = fill ? "fill" : "line"
+//   const nameIcon = name ? name : ""
+  
+//   return <i className={`ri-${nameIcon}-${fillIcon} ri-sm`}></i>
+// }
+
+// Icon.propTypes = {
+//   fill: PropTypes.bool,
+//   name: PropTypes.oneOf(['mail', 'user']),
+// }
+
+
+// // Add the Sidegit adbarItem component here
+// function SidebarItem(props) {
+//   const { children, href = "#", icon, current} = props
+//   const currentActive = current ? "sidebar-item--active" : ""
+
+//   return (
+//   <a 
+//   href={href} 
+//   className={`sidebar-item ${currentActive}`}
+//   >
+//     { icon }
+//     {children}
+//   </a>
+//   )
+// }
+
+// SidebarItem.propTypes = {
+//   href: PropTypes.string,
+//   current: PropTypes.bool
+// }
+
+// // Render UI
+// const ui = (
+//   <div className="">
+//     <div className="">
+//       <SidebarItem current={true} icon={<Icon name="mail" />}>
+//       Content Text</SidebarItem>
+//     </div>
+//   </div>
+// )
+
+// ReactDOM.render(<SidebarItem ...props />, document.getElementById("root"));
+// root.render(ui)
+
+function Icon(props) {
+const { name, fill } = props
+
+const nameIcon = name ? name : ""
+const fillIcon = fill ? "fill" : "line"
+
+  return( <i className={`ri-${nameIcon}-${fillIcon} ri-sm`}></i>)
 }
 
-// Add the Sidegit adbarItem component here
+Icon.propTypes = {
+  nameIcon: PropTypes.string,
+  fillIcon: PropTypes.string
+}
 
-// Render UI
+function SidebarItem(props) {
+  const {children, href = "#", current, icon } = props
+
+  const currentActive = current ? "sidebar-item:active" : ""
+
+  return (
+    <a href={href} className={`sidebar-item ${currentActive}`}>
+      {icon}
+      {children}
+    </a>
+  )
+}
+
+  SidebarItem.propTypes = {
+    current: PropTypes.bool,
+    href: PropTypes.string,
+    icon: PropTypes.element
+  }
+
+const ui = (
+  <div>
+    <SidebarItem current={true} href={"https://www.google.com"} icon={<Icon name="mail" fill="fill" />} >Link</SidebarItem>
+  </div>
+)
+
 root.render(ui)
-// ReactDOM.render(<SidebarItem ...props />, document.getElementById("root"));
